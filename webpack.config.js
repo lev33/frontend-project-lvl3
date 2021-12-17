f89ebdb5087c37b20233f62ac385cc3dcbeafca0
lev33 <48180783+lev33@users.noreply.github.com>
2021-12-17 10:03:37 +0300

    use hexlet webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
+
+/*
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
@@ -108,3 +138,4 @@ module.exports = {
     ],
   },
 };
+*/
