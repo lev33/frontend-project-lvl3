c40f231d143538f859723f57bd6db00e2b8c1869
lev33 <48180783+lev33@users.noreply.github.com>
2021-12-17 11:36:01 +0300

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: {
-    main: ['./src/index.js'],
-  },
   module: {
     rules: [
       {
